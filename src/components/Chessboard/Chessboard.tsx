d102ca745237589e9bdefd4392bde6d2ad2a7482
GabrielFQuaresma <work.gabriel.quaresma@gmail.com>
2024-10-29 20:16:03 -0300

    Add: Movement to Pieces

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -54,7 +54,53 @@ export default function Chessboard() {
             board.push(<Tile key={`${j},${i}`} coordinate={coordinate} image={image} />)
         }
     }
-    return <div id="chessboard">{board}</div>
+    return (
+        <div 
+            onMouseMove={e => movePiece(e)} 
+            onMouseDown={e => grabPiece(e)}
+            onMouseUp={e => dropPiece(e)}
+            id="chessboard">
+             {board}
+        
+        </div>);
+
+}
+
+let activePiece:HTMLElement | null = null;
+
+function dropPiece(e: React.MouseEvent){
+    if(activePiece) activePiece = null
+}
+
+function movePiece(e: React.MouseEvent){
+    const element = e.target as HTMLElement;
+    if(activePiece){
+
+        const x = e.clientX - 30;
+        const y = e.clientY - 30;
+        
+        activePiece.style.position = "absolute";
+        activePiece.style.left = `${x}px`
+        activePiece.style.top = `${y}px`
+
+        
+    }
+}
+
+function grabPiece(e: React.MouseEvent){
+    const element = e.target as HTMLElement;
+    if(element.classList.contains("chess-piece")){
+
+        const x = e.clientX - 30;
+        const y = e.clientY - 30;
+
+
+        element.style.position = "absolute";
+        element.style.left = `${x}px`
+        element.style.top = `${y}px`
+
+        activePiece = element;
+    }
 }
 
 export function setPieces() {
