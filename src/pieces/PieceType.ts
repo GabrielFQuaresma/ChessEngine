ac8a54d7442f859511ca21c2c1133c11772d30c0
GabrielFQuaresma <work.gabriel.quaresma@gmail.com>
2024-11-16 11:47:42 -0300

    Add: Image attribute

--- src/pieces/PieceType.ts
+++ src/pieces/PieceType.ts
@@ -3,12 +3,14 @@ import {colors} from "../components/Chessboard/Chessboard"
 export abstract class PieceType{
     x: number;
     y: number;
+    image: string;
     color: colors; //0 it means white, and 1 it means black
   
-    constructor(x: number, y: number, color: colors) {
+    constructor(x: number, y: number, image: string, color: colors) {
         this.x = x;
         this.y = y;
         this.color = color;
+        this.image = image;
     }
   
     // Método abstrato a ser implementado em subclasses
