bd72462ed6c852ad3149acaa20bd5ff4f5c410a6
GabrielFQuaresma <work.gabriel.quaresma@gmail.com>
2024-11-21 18:20:34 -0300

    Update: Changing the method to the abstract class and adding a new attribute

--- src/pieces/PieceType.ts
+++ src/pieces/PieceType.ts
@@ -1,21 +1,45 @@
-import {colors} from "../components/Chessboard/Chessboard"
+import {PiecesName} from "../components/Chessboard/Chessboard"
 
 export abstract class PieceType{
     x: number;
     y: number;
     image: string;
-    color: colors; //0 it means white, and 1 it means black
+    color: PiecesName; //0 it means white, and 1 it means black
+    type: PiecesName;
   
-    constructor(x: number, y: number, image: string, color: colors) {
+    constructor(x: number, y: number, image: string, color: PiecesName, type: PiecesName) {
         this.x = x;
         this.y = y;
-        this.color = color;
         this.image = image;
+        this.color = color;
+        this.type = type;
     }
   
     // Método abstrato a ser implementado em subclasses
     abstract isValidMove(newX: number, newY: number, occupied: bigint): boolean;
 
-    // abstract isPathClean(newX: number, newY: number, occupied: bigint): boolean;
+    isPathClean(newX: number, newY: number, occupied: bigint): boolean {
+
+        const directionRow = Math.sign(newY - this.y);
+        const directionCol = Math.sign(newX - this.x);
+
+        let currentRow = this.y + directionRow;
+        let currentCol = this.x + directionCol;
+
+        while (currentRow !== newY || currentCol !== newX) {
+            const bitPosition = 8 * currentRow + currentCol;
+            const mask = BigInt(1) << BigInt(bitPosition);
+            
+            if ((occupied & mask) !== BigInt(0)) {
+                return false; // Path is blocked
+            }
+
+            // Move to the next square in the direction
+            currentRow += directionRow;
+            currentCol += directionCol;
+        }
+
+        return true;
+    }
 
 }
