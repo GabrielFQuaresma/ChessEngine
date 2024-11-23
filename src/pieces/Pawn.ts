cbd085d49a3159f4bb283a54658c9302cce5f882
GabrielFQuaresma <work.gabriel.quaresma@gmail.com>
2024-11-23 20:29:01 -0300

    Fix: Black Pawn infinite jump

--- src/pieces/Pawn.ts
+++ src/pieces/Pawn.ts
@@ -8,12 +8,18 @@ export default class Pawn extends PieceType{
         
         const yInitial: number = (this.color === PiecesName.White) ? 1 : 6;
 
+        console.log("y: " + y + " newY: " + newY );
+
         if (!(this.isPathClean(newX, newY, occupied))) return false; 
 
-        if(y === yInitial){
-            if(newX === x && newY <= y + 2) return true;
-        }else{
-            if(newX === x && newY <= y + 1) return true;
+        if (newX === x) {
+            const maxMove = (y === yInitial) ? 2 : 1; // Pode mover até 2 casas na posição inicial, senão apenas 1.
+            
+            if (this.color === PiecesName.White) {
+                if (newY <= y + maxMove) return true; // Movimento para frente (brancas).
+            } else {
+                if (newY >= y - maxMove) return true; // Movimento para frente (pretas).
+            }
         }
         
         return false;
